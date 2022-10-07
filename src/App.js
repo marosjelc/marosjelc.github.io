8b12a99a598290eb0f0e6a2b67adc86ac8b5b1ed
Maroš Jelč <maros.jelc@gmail.com>
2022-10-07 13:37:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <h3>Hello there !</h3>
+      </section>
+    </div>
+  );
+}
+
+export default App;
